ca3c790d45ef08c093a6d614044c426fe6aa05a7
imteqoo <navareztalon@gmail.com>
2017-08-25 10:21:29 -1000

    finished SuperCar class

--- fantasy-vehicles.js
+++ fantasy-vehicles.js
@@ -5,14 +5,16 @@ class Vehicle {
     this.type = type;
   }
 
-  vehicleDetails() {
-    return 'Your vehicle is a ${this.type}, made by ${this.make}, and is ${this.color}';
+  get vehicleDetails() {
+    return `Your vehicle is a ${this.type}, made by ${this.make}, and is ${this.color}.`;
   }
 }
 
-let bike = new Vehicle('audi', 'red', 'motorized');
+let bike = new Vehicle('Audi', 'red', 'motorized');
 console.log(bike.vehicleDetails());
 
+
+// SuperCar is a subclass of Vehicle and has topSpeed and cost added
 class SuperCar extends Vehicle {
   constructor(make, color, type, topSpeed, cost) {
     super(make, color, type);
@@ -20,7 +22,19 @@ class SuperCar extends Vehicle {
     this._cost = cost;
   }
 
-  superCarDetails() {
-    return this.vehicleDetails()
+  get superCarDetails() {
+    return `${this.vehicleDetails()} It also has a top speed of ${this.topSpeed} and a cost of ${this.cost}.`
   } 
+
+  set priceTag(cost) {
+    if (typeof(cost) === 'number') {
+      cost = this._topSpeed;
+    } else {
+      throw new TypeError('SuperCar.cost must be a number!');
+    }
+  }
+
 }
+
+
+//
